9064f3456df1b9b739db437d50cb7315d57c60b5
madycakes <viclee02@gmail.com>
2019-06-12 13:15:03 -1000

    adds exercises

---
+++ mission.js
@@ -0,0 +1,42 @@
+//Mission 1: 
+//Declare a variable named zippys and assign it an array of 5 items sold at Zippys Restaurant.
+
+//Mission 2:
+//Declare a variable named giraffe and assign it an object with 5 key value pairs of your choosing.
+
+//Mission 3: 
+//Declare a variable named bank and assign to your favorite Hawaii bank.
+
+//Mission 4:
+//Declare a variable named price and assign the cost of a chicken katsu plate lunch from L&L Drive Inn.
+
+//Mission 5:
+//Declare a variable named weather and assign it tomorrow's weather forecast.
+
+//Mission 6:
+//Declare a variable named country and assign it an array of 3 countries that you've visited (or want to visit).
+
+//Mission 7:
+//Declare a variable named greeting and assign it to a Russian greeting for 'hello world'.
+
+//Mission 8:
+//Declare a variable named candyWrapper and assign it to a candy bar that has orange in its wrapper
+
+//Mission 9:
+//Declare a variable named beach and assign it to the number of beaches on Oahu.
+
+//Mission 10:
+//Declare a variable named middleName and assign it to the middle name of the person sitting next to you.
+
+//Mission 11:
+//Declare a variable named kicks and assign it to the brand of shoes that you are currently wearing. 
+
+//Mission 12:
+//Declare a variable named chips and assign it to the potato chips that are currently on sale at Longs Drugs. 
+
+//Mission 13:
+//Declare a variable named cheeseCake and assign it to the number of cheese cake flavors that CheeseCake Factory offers.
+
+//Mission 14:
+//Declare a variable named ronald and assign it to the number McDonald's restaurants in Hawaii.
+
