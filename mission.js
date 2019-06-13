d8c3089a5a55f71b5540df769d3b9e9be7fe40f4
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-06-12 21:42:09 -1000

    all dem cakes

--- mission.js
+++ mission.js
@@ -37,6 +37,8 @@
 //Mission 13: Daniel
 //Declare a variable named cheeseCake and assign it to the number of cheese cake flavors that CheeseCake Factory offers.
 
+var cheeseCake = 36;
+
 //Mission 14: Brian
 //Declare a variable named ronald and assign it to the number McDonald's restaurants in Hawaii.
 
